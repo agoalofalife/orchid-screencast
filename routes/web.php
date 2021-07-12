c2672f70060570e8f9bb05cb113ed05c550ee82e
agoalofalife <agoalofalife@gmail.com>
2021-07-12 13:42:58 +0300

    update read me

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     $notification = new \App\Notifications\NewsForOperator('Новый оператор в штат', 'Мы наняли нового оператора!');
 //    \App\Models\User::find(4)->notify($notification);
-    $users = \Orchid\Platform\Models\Role::find(3)->getUsers();
-    \Illuminate\Support\Facades\Notification::send($users, $notification);
+//    $users = \Orchid\Platform\Models\Role::find(3)->getUsers();
+//    \Illuminate\Support\Facades\Notification::send($users, $notification);
     return view('welcome');
 });
